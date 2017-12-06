a67e06232a69167fd6267ae52bfea0e349789a84
James Pierce <jpierce@us.ibm.com>
2017-12-06 08:35:14 -1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 #############################################
 
 provider "ibm" {
-  softlayer_username = "${var.slusername}"
-  softlayer_api_key = "${var.slapikey}"
+  softlayer_username = "${var.ibm_sl_username}"
+  softlayer_api_key = "${var.ibm_sl_api_key}"
 }
 
 ########################################################
