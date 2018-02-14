c734a83f8d10eb8c04d7dbd8df7687bb16913645
bashansh <shmuel.bashan@gmail.com>
2018-02-14 11:28:51 +0000

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -106,23 +106,23 @@ variable "ssh_key" {
 #    public_key = "${var.ssh_key}"
 #}
 
-resource "ibm_compute_vm_instance" "vm" {
-  hostname                 = "${var.hostname}"
-  os_reference_code        = "${var.os_reference_code}"
-  domain                   = "${var.domain}"
-  datacenter               = "${var.datacenter}"
-  network_speed            = "${var.network_speed}"
-  hourly_billing           = true
-  private_network_only     = "${var.private_network_only}"
-  cores                    = "${var.cores}"
-  memory                   = "${var.memory}"
-  disks                    = ["${var.disk_size}"]
-  dedicated_acct_host_only = true
-  local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
-  tags                     = ["${var.tags}"]
-  user_metadata            = "${file("install.yml")}"
-}
+#resource "ibm_compute_vm_instance" "vm" {
+#  hostname                 = "${var.hostname}"
+#  os_reference_code        = "${var.os_reference_code}"
+#  domain                   = "${var.domain}"
+#  datacenter               = "${var.datacenter}"
+#  network_speed            = "${var.network_speed}"
+#  hourly_billing           = true
+#  private_network_only     = "${var.private_network_only}"
+#  cores                    = "${var.cores}"
+#  memory                   = "${var.memory}"
+#  disks                    = ["${var.disk_size}"]
+#  dedicated_acct_host_only = true
+#  local_disk               = false
+#  ssh_key_ids              = ["${ibm_compute_ssh_key.ssh_key.id}"]
+#  tags                     = ["${var.tags}"]
+#  user_metadata            = "${file("install.yml")}"
+#}
 
 output "public_ip" {
 	value = "http://${ibm_compute_vm_instance.vm.ipv4_address}"
